16d3a4eae3e8df282c6aba4b09d0ceebb31422dc
poup <bourquin.julien@gmail.com>
2017-12-13 20:36:04 +0100

    add some plugins: - Anima2D pour faire des anim (unity) - Cinemachine: gestion de la camera qui tue (unity) - Energy Bar Toolkit: pour faire des jauges/ barres de vie/ etc... - JMO Assets: pack de particule sun peu cartoon - PostProcessing:  pack de shader de post process (unity) - TextFX : pour animer du texte - TextMesh Pro: pour avoir du texte joli(dégradé, contour, ombre, etc..) (unity) - WhiteCat: lib pour faire des courbes, suivre une courbes

---
+++ Assets/Plugins/WhiteCat/Common/CommonRuntime/BaseClasses/ScriptableComponent.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace WhiteCat
+{
+	/// <summary>
+	/// ScriptableComponent
+	/// </summary>
+	public class ScriptableComponent : MonoBehaviour
+	{
+		/// <summary>
+		/// RectTransform for UI scripting
+		/// </summary>
+		public RectTransform rectTransform
+		{
+			get { return transform as RectTransform; }
+		}
+
+	} // class ScriptableComponent
+
+} // namespace WhiteCat
