07cc2e8bff3ca767573bd1856a72c327dff8a106
Julien Bourquin <jbourquin@ankama.com>
2017-12-14 14:00:37 +0100

    base pour les rooms + player

---
+++ Assets/_Code/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private Rigidbody m_rigidBody;
+	[SerializeField] private float m_speedMax;
+	
+	private bool m_isJump; 
+
+	
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		var x = Input.GetAxis("Horizontal");
+		var y = Input.GetAxis("Vertical");
+		
+		m_rigidBody.velocity = new Vector3(x, 0, y) * m_speedMax;
+	}
+}
