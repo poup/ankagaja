eaf4890fe4fb760aab42725e230250666feaf136
Julien Bourquin <jbourquin@ankama.com>
2017-12-14 21:53:36 +0100

    add spawnZone that select any cells

--- Assets/_Code/Randomizer.cs
+++ Assets/_Code/Randomizer.cs
@@ -15,5 +15,30 @@ public static class Randomizer
         var index = m_random.Next(list.Count);
         return list[index];
     }
+    
+    public static T PickAndRemoveRandom<T>(this List<T> list)
+    {
+        var index = m_random.Next(list.Count);
+        var value = list[index];
+        list.RemoveAt(index);
+        return value;
+    }
+    
+    public static void Shuffle<T>(this List<T> list)
+    {
+        for (int i = list.Count; i > 1; --i)
+        {
+            var index = m_random.Next(i);
+            Swap(list, i - 1, index);
+        }
+    }
+
+
+    private static void Swap<T>(List<T> list, int i1, int i2)
+    {
+        var tmp = list[i1];
+        list[i1] = list[i2];
+        list[i2] = tmp;
+    }
 
 }
---
+++ Assets/_Code/Traps/SpawnZone/TrapSpawnZoneCells.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapSpawnZoneCells : TrapSpawnZone
+{
+    [SerializeField] private float m_width;
+    [SerializeField] private float m_height;
+    
+    private readonly List<Vector2> m_remains = new List<Vector2>();
+
+    private void Awake()
+    {
+        Reset();
+    }
+
+    public void OnDisable()
+    {
+        Reset();
+    }
+    
+    private void OnDrawGizmos()
+    {
+        var size = new Vector3(m_width, m_height, 0);
+        Gizmos.DrawWireCube(transform.position, size);
+    }
+
+    public override Vector2 GetPosition()
+    {
+        return m_remains.PickAndRemoveRandom();
+    }
+    
+    public void Reset()
+    {
+        m_remains.Clear();
+
+        var maxX = Mathf.CeilToInt(m_width);
+        var maxY = Mathf.CeilToInt(m_height);
+        for (int x = 0; x < maxX; ++x)
+        for (int y = 0; y < maxY; ++y)
+        {
+            m_remains.Add(new Vector2(x+0.5f, y+0.5f));
+        }
+
+        m_remains.Shuffle();
+    }
+}
