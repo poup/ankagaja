0c00ec091e060117347ef7a2062f3e6d8ed8c5e0
Julien Bourquin <jbourquin@ankama.com>
2017-12-14 22:23:13 +0100

    add jump + dash (WIP)

--- Assets/_Code/PlayerController.cs
+++ Assets/_Code/PlayerController.cs
@@ -3,14 +3,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+	[SerializeField] private Transform m_characterRenderTransform;
 	[SerializeField] private Rigidbody2D m_rigidBody;
 	[SerializeField] private Animator m_animator;
 	[SerializeField] private float m_speedMax = 12.0f;
 	[SerializeField] private float m_acceleration = 600.0f;
 
 	[SerializeField] private float m_playerContactFactor = 1.0f;
+	
 	[SerializeField] private float m_dashDuration = 2.0f;
-	private float m_dashTime = 0.0f;
+	[SerializeField] private float m_dashSpeedBonus = 0.0f;
+	[SerializeField] private float m_dashWeightBonus = 0.0f;
+	private float m_dashTime = -1.0f;
+	
+	
+	[SerializeField] private float m_jumpDuration = 1.0f;
+	[SerializeField] private AnimationCurve m_jumpCurve = new AnimationCurve();
+	private float m_jumpTime = -1.0f;
 
 	private float m_moveX;
 	private float m_moveY;
@@ -22,11 +31,24 @@ public class PlayerController : MonoBehaviour
 
 
 	private ContactPoint2D m_contact;
+	
+	public bool IsJumping { get { return m_jumpTime >= 0;  } }
+	public bool IsDashing { get { return m_dashTime >= 0;  } }
+
+	public float JumpingHeight
+	{
+		get
+		{
+			if (!IsJumping)
+				return 0;
+			return m_jumpCurve.Evaluate(m_jumpTime / m_jumpDuration);
+		}
+	}
 
 
 	void Update()
 	{
-#if! TOTO
+#if TOTO
 		var x = Input.H1();
 		var y = Input.V1();
 
@@ -37,14 +59,15 @@ public class PlayerController : MonoBehaviour
 		var y = UnityEngine.Input.GetAxis("Vertical") ;
 
 
-		var jump = false;//Input.A();
-		var dash = false; // Input.X();
-		#endif
+		var jump = UnityEngine.Input.GetButtonDown("Jump") ;
+		var dash = UnityEngine.Input.GetButtonDown("Fire1") ;
+#endif
 
+		var dt = Time.deltaTime;
+		DashingUpdate(dash, dt);
+		JumpingUpdate(jump, dt);
 
-		var isMoving = Mathf.Abs(x) > 0.01f || Mathf.Abs(y) > 0.01f;
-		var isJumping = false; // TODO
-		var isDashing = false; // TODO
+		var isMoving = Mathf.Abs(x) > 0.01f || Mathf.Abs(y) > 0.01f || IsDashing;
 
 		if (isMoving)
 		{
@@ -52,8 +75,25 @@ public class PlayerController : MonoBehaviour
 			m_dirY = y;
 		}
 
-		m_moveX = x;
-		m_moveY = y;
+
+//		var isMoving = Mathf.Abs(x) > 0.001f || Mathf.Abs(y) > 0.001f;
+//		var isJumping = false; // TODO
+//		var isDashing = false; // TODO
+//
+//		if (Mathf.Abs(x) > 0.001f)
+//		{
+//			m_dirX = x;
+//		}
+//		if (Mathf.Abs(y) > 0.001f)
+//		{
+//			m_dirY = y;
+//		}
+
+		if (!IsDashing)
+		{
+			m_moveX = x;
+			m_moveY = y;
+		}
 
 		m_animator.SetFloat("moveX", x);
 		m_animator.SetFloat("moveY", y);
@@ -61,8 +101,52 @@ public class PlayerController : MonoBehaviour
 		m_animator.SetFloat("dirY", m_dirY);
 
 		m_animator.SetBool("isMoving", isMoving);
-		m_animator.SetBool("isJumping", isJumping);
-		m_animator.SetBool("isDashing", isDashing);
+		m_animator.SetBool("isJumping", IsJumping);
+		m_animator.SetBool("isDashing", IsDashing);
+	}
+
+	private void DashingUpdate(bool dash, float dt)
+	{
+		var dashing = IsDashing;
+		if (!dashing && dash)
+		{
+			m_dashTime = 0.0f;
+			dashing = true;
+		}
+		
+		if (dashing)
+		{
+			m_dashTime += dt;
+		}
+		if (m_dashTime > m_dashDuration)
+		{
+			m_dashTime = -1;
+		}
+	}
+
+	private void JumpingUpdate(bool jump, float dt)
+	{
+		if (IsDashing)
+			return;
+		
+		var jumping = IsJumping;
+		if (!jumping && jump)
+		{
+			m_jumpTime = 0.0f;
+			jumping = true;
+		}
+		
+		if (jumping)
+		{
+			m_jumpTime += dt; 
+			if (m_jumpTime > m_jumpDuration)
+			{
+				m_jumpTime = -1;
+			}
+		}
+
+		var p = m_characterRenderTransform.localPosition;
+		m_characterRenderTransform.localPosition = new Vector3(p.x, JumpingHeight, p.z);
 	}
 
 	private void FixedUpdate()
