4f0a3d450b73bba8b080588e866489459250dd17
Julien Bourquin <jbourquin@ankama.com>
2017-12-15 19:37:28 +0100

    ia in night can kill

--- Assets/_Code/Traps/Spawner/TrapGlobalNight.cs
+++ Assets/_Code/Traps/Spawner/TrapGlobalNight.cs
@@ -1,43 +1,32 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Assets._Code;
 using UnityEngine;
 
-public class TrapGlobalNight : BaseTrap
+public class TrapGlobalNight : TrapSpawner
 {
-    [SerializeField] protected float m_durationBeforeActivation = 0;
     [SerializeField] protected float m_waitFullBlack = 0.8f;
-    
+
     [SerializeField] protected float m_fadeInDuration = 0.3f;
     [SerializeField] protected float m_fadeOutDuration = 0.3f;
-    
+
     [SerializeField] protected float m_duration = 8.0f;
-    
-    [SerializeField] private string m_inNightStateName;
+
     [SerializeField] private SpriteRenderer m_nightFilter;
-    [SerializeField] private GameObject m_iaPrefab;
     [SerializeField] private int m_iaCount;
-    TrapSpawnZone[] m_spawnZones;
     
+    [Space(10)]
+    [SerializeField] protected TrapActions.TrapActionsType m_actionOnTrigger;
 
 
-    protected virtual void Awake()
-    {
-        m_spawnZones = GetComponentsInChildren<TrapSpawnZone>();
-        if (m_spawnZones == null || m_spawnZones.Length == 0)
-            throw new Exception("faut mettre au moins une spawnZone");
-    }
-
-    
-
-  
     protected virtual IEnumerator Start()
     {
         var players = FindObjectsOfType<PlayerController>();
-        yield return new WaitForSeconds(m_durationBeforeActivation);
+        yield return new WaitForSeconds(m_beforeSpawnDuration);
 
         yield return StartCoroutine(NightFade(0, 1, m_fadeInDuration));
-        
+
         yield return new WaitForSeconds(m_waitFullBlack);
         foreach (var p in players)
         {
@@ -53,23 +42,26 @@ public class TrapGlobalNight : BaseTrap
         {
             p.ShowGrosYeux(false);
         }
-        
     }
 
+    protected override IEnumerator Spawn()
+    {
+        throw new NotImplementedException();
+    }
 
     private List<GameObject> AddIA()
     {
         List<GameObject> list = new List<GameObject>();
         for (int i = 0; i < m_iaCount; ++i)
         {
-            var pos = m_spawnZones.PickRandom().GetPosition();
-            var go = Instantiate(m_iaPrefab, pos, Quaternion.identity, transform);
-            list.Add(go);
+            var obj = SpawnOne();
+            obj.OnTriggerEnter += ActivateKillingBox;
+            list.Add(obj.gameObject);
         }
 
         return list;
     }
-    
+
     private void RemoveIA(List<GameObject> list)
     {
         foreach (var go in list)
@@ -88,9 +80,13 @@ public class TrapGlobalNight : BaseTrap
             yield return frame;
             time += Time.deltaTime;
 
-            color.a = Mathf.Lerp(from, to, time/duration);
+            color.a = Mathf.Lerp(from, to, time / duration);
             m_nightFilter.color = color;
         }
-           
+    }
+
+    private void ActivateKillingBox(GameObject triggered, Collider2D triggerer)
+    {
+        TrapActions.DoAction(this, m_actionOnTrigger, triggered, triggerer);
     }
 }
