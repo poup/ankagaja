cf6536835a98cb161b025ace96d65a9f609159b3
Julien Bourquin <jbourquin@ankama.com>
2017-12-15 20:12:29 +0100

    une seul fois le même trap dans la même room

--- Assets/_Code/Traps/TrapManager.cs
+++ Assets/_Code/Traps/TrapManager.cs
@@ -5,6 +5,7 @@ public class TrapManager : MonoBehaviour
 {
 
 	[SerializeField] private List<BaseTrap> m_traps = new List<BaseTrap>();
+	private List<BaseTrap> m_trapsForRoom = new List<BaseTrap>();
 
 	private static TrapManager m_instance;
 	
@@ -18,20 +19,37 @@ public class TrapManager : MonoBehaviour
 		m_instance = this;
 	}
 
+	private void Start()
+	{
+		ResetAvailableTraps();
+	}
+
 	public BaseTrap GetRandomTrap()
 	{
-		return m_traps.PickRandom();
+		if(m_trapsForRoom.Count == 0)
+			ResetAvailableTraps();
+		
+		return m_trapsForRoom.PickAndRemoveRandom();
 	}
 
 	public void StartTrap()
 	{
-		if (m_traps.Count == 0)
+		if (m_trapsForRoom.Count == 0)
 			return;
 		var trap = GetRandomTrap();
 		Instantiate(trap, Vector3.zero, Quaternion.identity, transform);
 		
 		Debug.Log("trap started");
 	}
-	
-	
+
+
+	public void RoomEnded()
+	{
+		ResetAvailableTraps();
+	}
+
+	private void ResetAvailableTraps()
+	{
+		m_trapsForRoom = new List<BaseTrap>(m_traps);
+	}
 }
