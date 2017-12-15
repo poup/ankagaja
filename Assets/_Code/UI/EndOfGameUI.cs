78eaa41cc59233d3a52fbf7d607ec75b7bb39337
Julien Bourquin <jbourquin@ankama.com>
2017-12-15 21:26:02 +0100

    score a la fin

--- Assets/_Code/UI/EndOfGameUI.cs
+++ Assets/_Code/UI/EndOfGameUI.cs
@@ -1,22 +1,36 @@
-﻿using States;
+﻿using System;
+using DefaultNamespace;
+using States;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Assets._Code.UI
 {
-	public class EndOfGameUI : MonoBehaviour
-	{
-		public Text m_score1;
-		public Text m_score2;
-		public Text m_score3;
-		public Text m_score4;
-		
-		private void Update()
-		{
-			if (InputsManager.Instance.MainPlayerInput.Get.A())
-			{
-				FSM.Instance.GotoState<LobbyState>();
-			}
-		}
-	}
+    public class EndOfGameUI : MonoBehaviour
+    {
+        public Text m_score1;
+        public Text m_score2;
+        public Text m_score3;
+        public Text m_score4;
+
+        void Start()
+        {
+            m_score1.text =m_score2.text =m_score3.text = m_score4.text = "";
+            try
+            {
+                m_score1.text = PlayersManager.Instance.Players[0].Value.ToString();
+                m_score2.text = PlayersManager.Instance.Players[1].Value.ToString();
+                m_score3.text = PlayersManager.Instance.Players[2].Value.ToString();
+                m_score4.text = PlayersManager.Instance.Players[3].Value.ToString();
+            }catch(Exception ex) {}
+        }
+
+        private void Update()
+        {
+            if (InputsManager.Instance.MainPlayerInput.Get.A())
+            {
+                FSM.Instance.GotoState<LobbyState>();
+            }
+        }
+    }
 }
