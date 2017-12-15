5ee3ef0854c6550fee48ec185d16c4e278c7f974
Julien Bourquin <jbourquin@ankama.com>
2017-12-15 02:43:52 +0100

    some behaviour to move objects (random move for IA / scie qui se déplace )

---
+++ Assets/_Code/Movement/MoveInLine.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MoveInLine : MonoBehaviour
+{
+    public WrapMode m_wrapMode;
+    public Vector2 m_direction;
+    public float m_speed;
+    public float m_duration;
+
+    public Rigidbody2D m_rigidBody;
+    
+    
+    
+
+    void FixedUpdate()
+    {
+         // TODO   
+    }
+}
---
+++ Assets/_Code/Movement/RandomMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+    public class RandomMove : MonoBehaviour
+    {
+        public float m_timeBeforeChangeDirectionMin;
+        public float m_timeBeforeChangeDirectionMax;
+        
+        public float m_speedMin;
+        public float m_speedMax;
+        
+        public float m_timeMovingMin;
+        public float m_timeMovingMax;
+
+        private Rigidbody2D m_rigidbody2D;
+
+
+        void Start()
+        {
+            StartMove();
+        }
+
+        void StartMove()
+        {
+            StartCoroutine(Move());
+        }
+
+        private IEnumerator Move()
+        {
+            var waiting = Randomizer.Next(m_timeBeforeChangeDirectionMin, m_timeBeforeChangeDirectionMax);
+            yield return new WaitForSeconds(waiting);
+            var direction = Random.insideUnitCircle.normalized;
+
+            var speed = Randomizer.Next(m_speedMin, m_speedMax);
+            m_rigidbody2D.velocity = direction * speed;
+            
+            var movingTime = Randomizer.Next(m_timeMovingMin, m_timeMovingMax);
+            yield return new WaitForSeconds(movingTime);
+            
+            m_rigidbody2D.velocity = Vector2.zero;
+            
+        }
+    }
