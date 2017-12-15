286d20e5468691da47e6640e29febfa0d0ce7fb9
Julien Bourquin <jbourquin@ankama.com>
2017-12-15 21:19:37 +0100

    page de fin

--- Assets/_Code/UI/EndOfGameUI.cs
+++ Assets/_Code/UI/EndOfGameUI.cs
@@ -1,10 +1,16 @@
 ﻿using States;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets._Code.UI
 {
 	public class EndOfGameUI : MonoBehaviour
 	{
+		public Text m_score1;
+		public Text m_score2;
+		public Text m_score3;
+		public Text m_score4;
+		
 		private void Update()
 		{
 			if (InputsManager.Instance.MainPlayerInput.Get.A())
